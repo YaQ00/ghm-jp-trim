5ab98bd9f0f4574cf94845a5e0e4ac7d9c8fb5b5
yaq00 <yaq00@konapc>
2019-04-18 05:50:35 +0900

    fix

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/', (req, res) => {
-    const text = req.body.status.replace(/\s+/g, '')
-    request.post("https://misskey.io/api/notes/create", { form: { text: text, i: req.body.access_token } })
-    console.log(text)
+    const state = req.body.text.replace(/\s+/g, '')
+    request.post("https://misskey.io/api/notes/create", { form: { text: text, i: req.body.i } })
+    console.log(state)
     res
         .status(200)
-        .send(text)
+        .send(state)
         .end();
 });
 const PORT = process.env.PORT || 8080;
